25aee095dacee7dd70dfd54137d967fc229e46fe
Pelmato <ctt-github@zygalio.net>
2025-06-19 15:27:58 +0200

    fish as default shell

---
+++ roles/fish/files/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# NVM https://github.com/nvm-sh/nvm/issues/303
+function nvm
+    bash -c "source ~/.nvm/nvm.sh; nvm $argv"
+end
+set -gx PATH $HOME/.nvm/versions/node/v20.17.0/bin $PATH
+
+
+# SDKMAN https://github.com/sdkman/sdkman-cli/issues/671
+function sdk
+    bash -c "source '$HOME/.sdkman/bin/sdkman-init.sh'; sdk $argv[1..]"
+end
+fish_add_path (find ~/.sdkman/candidates/*/current/bin -maxdepth 0)
